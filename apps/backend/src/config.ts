58f00d1de8c8394b8386916210d52b45e885fea7
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-06-22 22:30:16 +0530

    nodemailer credential type fixed!

--- apps/backend/src/config.ts
+++ apps/backend/src/config.ts
@@ -6,7 +6,7 @@ export const JWT_USER_SECRET = process.env.JWT_USER_SECRET as string;
 export const FRONTEND_URL = process.env.FRONTEND_URL as string;
 
 export const OTP_SENDERMAIL = process.env.OTP_SENDERMAIL;
-export const OTP_MAIL_PASSWORD = process.env.ADMINMAIL_PASSWORD;
+export const OTP_MAIL_PASSWORD = process.env.OTP_MAIL_PASSWORD;
 
 export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
--- apps/backend/src/routes/userRoutes.ts
+++ apps/backend/src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { logout, session, signin, signup, verify_email } from "../controllers/userControllers";
 import { UserAuth } from "../middlewares/userAuthentication";
+import { requestValidation } from "../middlewares/requestValidation";
 
 export const UserRouter = Router();
 
-UserRouter.post("/signup", signup);
-UserRouter.post("/signin", signin);
+UserRouter.post("/signup" , requestValidation , signup);
+UserRouter.post("/signin" , requestValidation , signin);
 UserRouter.post("/logout", logout)
-UserRouter.post("/verify-mail", verify_email);
+UserRouter.post("/verify-mail" , requestValidation , verify_email);
 UserRouter.get("/session", UserAuth, session);
