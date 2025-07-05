71f253b3851a90131ce46e174b6dc933c7dbac01
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-07-05 14:40:49 +0530

    fixed react-toast-notification issue, adding toaster in layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,19 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Toaster
+          position="top-center"
+          containerStyle={{
+            zIndex: 999999 // Higher than your modal's z-index
+          }}
+          toastOptions={{
+            style: {
+              background: '#363636',
+              color: '#fff',
+              zIndex: 999999
+            },
+          }}
+        />
       </body>
     </html>
   );
