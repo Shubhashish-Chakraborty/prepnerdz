9fab4e71f307bff6f11b8d31b4cfdab051819f0c
raghavendra-24 <raghavendrapalagani671@gmail.com>
2025-07-31 18:38:23 +0530

    Added leaderboard Feature

--- apps/web/components/ui/navbars/Navbar.tsx
+++ apps/web/components/ui/navbars/Navbar.tsx
@@ -165,6 +165,16 @@ export const Navbar = () => {
                                     </span>
                                 </Link>
 
+                                <Link
+                                    href="/leaderboard"
+                                    className="inline-flex h-9 w-max items-center justify-center rounded-md bg-transparent px-3 xl:px-4 py-2 text-base xl:text-lg font-medium text-gray-900 transition-colors hover:bg-white/20 hover:text-gray-900 focus:bg-white/20 focus:text-gray-900 focus:outline-none"
+                                    onClick={closeAllDropdowns}
+                                >
+                                    <span className="hover:text-black hover:bg-amber-300 rounded-2xl p-1 transition-all duration-300 hover:scale-110">
+                                        Leaderboard
+                                    </span>
+                                </Link>
+
                                 {/* StudyMaterial Dropdown */}
                                 <div className="relative">
                                     <button
