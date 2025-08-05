0146e8b055d997c130e1fdb3a33243a1e8159abc
M Mohammad Anas <mohammad.anas51@hotmail.com>
2025-08-05 10:20:59 +0530

    fixed the content behind the sidebar going black after logging in in mobile view

--- apps/web/components/ui/sidebars/Sidebar.tsx
+++ apps/web/components/ui/sidebars/Sidebar.tsx
@@ -80,6 +80,7 @@ export default function Sidebar({
                 </div>
             </div>
 
+                        
             {/* Mobile Sidebar */}
             <div
                 className={`lg:hidden fixed inset-y-0 left-0 z-50 w-64 bg-white border-r border-gray-200 shadow-lg transform transition-transform duration-300 ease-in-out ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
