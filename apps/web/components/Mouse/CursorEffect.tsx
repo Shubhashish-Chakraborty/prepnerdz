e289df88a704833787b1c8bf6d8db9ba7ebbb90e
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-08-22 20:17:31 +0530

    build errors fixed

--- apps/web/components/Mouse/CursorEffect.tsx
+++ apps/web/components/Mouse/CursorEffect.tsx
@@ -1,44 +1,44 @@
-"use client";
+// "use client";
 
-import { Toaster } from "react-hot-toast";
-import BackToTopButton from "../ui/BackToTopButton";
-import AskNerd from "@/components/ui/AskNerd";
-import GssocBanner from "@/components/ui/GssocBanner";
-import { useEffect } from "react";
+// import { Toaster } from "react-hot-toast";
+// import BackToTopButton from "../ui/BackToTopButton";
+// import AskNerd from "@/components/ui/AskNerd";
+// import GssocBanner from "@/components/ui/GssocBanner";
+// import { useEffect } from "react";
 
-export default function CursorEffect({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  useEffect(() => {
-    const cursor = document.querySelector(".cursor");
-    document.addEventListener("mousemove", (e) => {
-      cursor.style.top = e.pageY - 10 + "px";
-      cursor.style.left = e.pageX - 10 + "px";
-    });
-  }, []);
+// export default function CursorEffect({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   useEffect(() => {
+//     const cursor = document.querySelector(".cursor");
+//     document.addEventListener("mousemove", (e) => {
+//       cursor.style.top = e.pageY - 10 + "px";
+//       cursor.style.left = e.pageX - 10 + "px";
+//     });
+//   }, []);
 
-  return (
-    <>
-      <GssocBanner />
-      {children}
-      <AskNerd />
-      <BackToTopButton />
-      <Toaster
-        position="top-center"
-        containerStyle={{
-          zIndex: 999999,
-        }}
-        toastOptions={{
-          style: {
-            background: "#363636",
-            color: "#fff",
-            zIndex: 999999,
-          },
-        }}
-      />
-      <div className="cursor" />
-    </>
-  );
-}
+//   return (
+//     <>
+//       <GssocBanner />
+//       {children}
+//       <AskNerd />
+//       <BackToTopButton />
+//       <Toaster
+//         position="top-center"
+//         containerStyle={{
+//           zIndex: 999999,
+//         }}
+//         toastOptions={{
+//           style: {
+//             background: "#363636",
+//             color: "#fff",
+//             zIndex: 999999,
+//           },
+//         }}
+//       />
+//       <div className="cursor" />
+//     </>
+//   );
+// }
